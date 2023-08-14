c5893286a2ec114ee3dfb25061e40602a1496881
Maab0Mubarak <oohyerim480@gmail.com>
2023-08-13 17:23:14 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,11 +13,12 @@ int main(void)
 {
 	char ch = 'a';
 
-	whaile (ch = 'a'))
-{
-	     putchar (ch)r
-      	     ch++;
-}
-         putchar ('\n')
-	 return (0)
+	while (ch <= 'z')
+	{
+	     putchar (ch);
+	     ch++;
+	}
+	putchar('\n');
+	
+	return (0);;
 }
